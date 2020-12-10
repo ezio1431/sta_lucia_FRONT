13c681a44b00907141dea0d6af8d4c36923efe29
Kevin <geykure@gmail.com>
2020-12-10 12:58:21 +0300

    Updated core and cli

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -76,8 +76,9 @@ const routes: Routes = [
     CommonModule,
     BrowserModule,
     RouterModule.forRoot(routes, {
-       useHash: true
-    })
+    useHash: true,
+    relativeLinkResolution: 'legacy'
+})
   ],
   exports: [
   ],
