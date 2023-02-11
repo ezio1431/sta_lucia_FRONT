0c45be7a46aa7f7e64ab5feca5c33e25a6e3e71c
Kevin <geykure@gmail.com>
2023-02-11 21:07:39 +0300

    Translated utilities

--- src/app/invoices/invoice-routing.module.ts
+++ src/app/invoices/invoice-routing.module.ts
@@ -2,7 +2,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { InvoiceComponent } from './invoice.component';
 import { AddInvoiceComponent } from './add/add-invoice.component';
 import { ViewInvoiceComponent } from './view/view-invoice.component';
-import { ViewInvoiceGeneralComponent } from './view/general/view-invoice-general.component';
 
 export const ROUTES: Routes = [
     {
@@ -12,10 +11,7 @@ export const ROUTES: Routes = [
     { path: 'create', component: AddInvoiceComponent },
     {
         path: ':id',
-        component: ViewInvoiceComponent,
-        children: [
-            { path: '', component: ViewInvoiceGeneralComponent }
-        ]
+        component: ViewInvoiceComponent
     },
 ];
 
--- src/app/invoices/invoice.module.ts
+++ src/app/invoices/invoice.module.ts
@@ -3,7 +3,6 @@ import { InvoiceRoutingModule } from './invoice-routing.module';
 import { InvoiceComponent } from './invoice.component';
 import { AddInvoiceComponent } from './add/add-invoice.component';
 import { SharedModule } from '../shared/shared.module';
-import { ViewInvoiceGeneralComponent } from './view/general/view-invoice-general.component';
 import { ViewInvoiceComponent } from './view/view-invoice.component';
 import { UtilityBillUnitDetailsComponent } from './add/unit-details/utility-bill-unit-details.component';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
@@ -18,7 +17,6 @@ import { WaiveInvoiceComponent } from './view/waive/waive-invoice.component';
     declarations: [
         InvoiceComponent,
         AddInvoiceComponent,
-        ViewInvoiceGeneralComponent,
         ViewInvoiceComponent,
         UtilityBillUnitDetailsComponent,
         WaiveInvoiceComponent
